a07c23eb3b38966d8c673d0e430708a0a82bc01c
alyssoncs <alysson.cirilo@gmail.com>
2017-08-09 19:06:49 -0300

    prim

--- src/main.c
+++ src/main.c
@@ -3,20 +3,23 @@
 #include "graphit.h"
 
 int main(int argc, char const *argv[]) {
-	size_t v, s, a, b;
+	size_t v, e, a, b;
 	double w;
 
 	scanf("%zu\n", &v);
-	scanf("%zu\n", &s);
+	scanf("%zu\n", &e);
 	graph *g = create_weighted_graph(v);
 	if (g)
 	{
 		while (scanf("%zu %zu %lf\n", &a, &b, &w) != EOF)
+		while (e--)
 		{
+			scanf("%zu %zu %lf\n", &a, &b, &w)
 			add_edge(g, a, b, w);
+			add_edge(g, b, a, w);
 		}
 
-		double *cost = dijkstra(g, s);
+		double *cost = dijkstra(g, 0);
 		if (cost)
 		{
 			size_t i;
